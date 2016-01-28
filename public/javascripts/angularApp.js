96fcf99f7c9cf3e757a7efee0949f2fd8d79108d
Felipe Wilke <felipewilke@gmail.com>
2016-01-28 11:18:13 -0200

    Preventing empty title value on form submit

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -14,6 +14,7 @@ function($scope){
   ];
 
   $scope.addPost = function(){
+  	if(!$scope.title || $scope.title === '') { return; }
 	$scope.posts.push({title: $scope.title, upvotes: 0});
 	$scope.title = '';
   };
