8446d8094507c2f039c191228150b83fe8b37b41
Felipe Wilke <felipewilke@gmail.com>
2016-01-28 11:16:40 -0200

    Posting with form

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -6,15 +6,16 @@ function($scope){
   $scope.test = 'Hello world!';
 
   $scope.posts = [
-  {title: 'post 1', upvotes: 5},
-  {title: 'post 2', upvotes: 2},
-  {title: 'post 3', upvotes: 15},
-  {title: 'post 4', upvotes: 9},
-  {title: 'post 5', upvotes: 4}
+	  {title: 'post 1', upvotes: 5},
+	  {title: 'post 2', upvotes: 2},
+	  {title: 'post 3', upvotes: 15},
+	  {title: 'post 4', upvotes: 9},
+	  {title: 'post 5', upvotes: 4}
   ];
 
   $scope.addPost = function(){
-  $scope.posts.push({title: 'A new post!', upvotes: 0});
+	$scope.posts.push({title: $scope.title, upvotes: 0});
+	$scope.title = '';
   };
 
 }]);
