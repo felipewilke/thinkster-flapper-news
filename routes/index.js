0ed79a16fa00c55be926b3bef6ecc406938d0c31
Felipe Wilke <felipewilke@gmail.com>
2016-01-28 15:48:48 -0200

    get post by id parameter route added

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Posts routes */
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
+
+    req.post = post;
+    return next();
+  });
+});
+
+router.get('/posts/:post', function(req, res) {
+  res.json(req.post);
+});
+
 router.get('/posts', function(req, res, next) {
   Post.find(function(err, posts){
     if(err){ return next(err); }
