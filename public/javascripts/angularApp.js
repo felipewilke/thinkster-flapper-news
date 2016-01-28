9ba961d86834b3747df22ad59484578e306edddf
Felipe Wilke <felipewilke@gmail.com>
2016-01-28 14:31:43 -0200

    Post mock comments and route added

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -20,7 +20,11 @@ function($scope, posts){
 	$scope.posts.push({
 		title: $scope.title,
 		link: $scope.link, 
-		upvotes: 0
+		upvotes: 0,
+		comments: [
+		    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+		    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+	  	]
 	});
 	$scope.title = '';
 	$scope.link = '';
@@ -29,6 +33,15 @@ function($scope, posts){
   $scope.incrementUpvotes = function(post) {
   	post.upvotes += 1;
   };
+
+}]);
+
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+	$scope.post = posts.posts[$stateParams.id];
 
 }]);
 
@@ -42,7 +55,12 @@ function($stateProvider, $urlRouterProvider) {
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl'
-    });
+    })
+    .state('posts', {
+	  url: '/posts/{id}',
+	  templateUrl: '/posts.html',
+	  controller: 'PostsCtrl'
+	});
 
   $urlRouterProvider.otherwise('home');
 }]);
