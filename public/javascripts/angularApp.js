56d3b6cae43efb625f5099c005fd87acd6bc2231
Felipe Wilke <felipewilke@gmail.com>
2016-01-29 09:31:04 -0200

    Creating new posts

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -4,11 +4,19 @@ app.factory('posts', ['$http', function($http){
   var o = {
     posts: []
   };
+
   o.getAll = function() {
     return $http.get('/posts').success(function(data){
       angular.copy(data, o.posts);
     });
   };
+
+  o.create = function(post) {
+	return $http.post('/posts', post).success(function(data){
+		o.posts.push(data);
+	});
+  };
+
   return o;
 }]);
 
@@ -16,20 +24,13 @@ app.controller('MainCtrl', [
 '$scope',
 'posts',
 function($scope, posts){
-  $scope.test = 'Hello world!';
-
   $scope.posts = posts.posts;
 
   $scope.addPost = function(){
   	if(!$scope.title || $scope.title === '') { return; }
-	$scope.posts.push({
-		title: $scope.title,
-		link: $scope.link, 
-		upvotes: 0,
-		comments: [
-		    {author: 'Joe', body: 'Cool post!', upvotes: 0},
-		    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-	  	]
+	posts.create({
+	    title: $scope.title,
+	    link: $scope.link,
 	});
 	$scope.title = '';
 	$scope.link = '';
